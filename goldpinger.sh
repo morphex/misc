aabad829cd67b7c82d9d24112fb3fea9a5f9f923
Morten W. Petersen <morphex@gmail.com>
2020-11-12 07:05:32 +0100

    Making script more readable and quiet.

--- goldpinger.sh
+++ goldpinger.sh
@@ -8,17 +8,17 @@
 #
 # Has to be run as root.
 
-r=`route|grep default|awk '{print $8}'`
+# Getting the active interface
+interface=`route|grep default|awk '{print $8}'`
 while :
 do
-  ping -c 3 8.8.8.8
+  ping -c 3 8.8.8.8 > /dev/null 2>&1
   if [ $? -eq 0 ];
   then
-    echo "All is well"
+    :
   else
     echo "Ping failed, regetting IP with dhclient"
-    /sbin/dhclient $r
+    /sbin/dhclient $interface
   fi
   sleep 10s
 done
-
