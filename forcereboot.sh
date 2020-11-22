35b72fa01df91f36a963aa6e77d33d7461238e56
Morten W. Petersen <morphex@gmail.com>
2020-11-22 11:53:50 +0100

    Adding forced reboot script

---
+++ forcereboot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Script that will force a reboot, regardless of the state of things.  Only
+# tries to synchronize the file systems, wait 5 seconds, then remount
+# all file systems read only, wait 5 seconds, then reboot.
+#
+# Should be safe to setuid root.
+
+# Try to synchronize filesystem state
+/bin/sync &
+sync_pid=$!
+/bin/sleep 5s
+# Remount all mounted filesystems read only
+echo u > /proc/sysrq-trigger
+/bin/sleep 5s
+# Force an immediate reboot
+/sbin/reboot -ff
