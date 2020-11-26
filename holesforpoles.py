730cdd4cd0ecfe706cb1075be5a394395fe6d6a2
Morten W. Petersen <morphex@gmail.com>
2020-11-26 15:11:58 +0100

    Cleaning up output

--- holesforpoles.py
+++ holesforpoles.py
@@ -3,7 +3,7 @@
 __doc__ = """
 
 Small utility script to find the right places to put
-to poles that will support a roof, so that it is possible
+two poles that will support a roof, so that it is possible
 to extend it to a whole length with an extra pole.
 
 """
@@ -12,11 +12,24 @@ to extend it to a whole length with an extra pole.
 whole = 7.0
 partial = 4.5
 
-for x in range(50,150,5):
+# Centimetres
+start = 50
+stop = 150
+step = 5
+
+# Acceptable difference, centimetres
+imprecision = 10
+
+for x in range(start,stop,step):
   x = x / 100.0
-  print(x)
+  imprecision = imprecision / 100.0
   partial_ = partial - (x*2)
   whole_ = whole - (x*2)
-  print(str(partial_) + " " + str(whole_))
-  print(str(partial_) + " " + str(whole_ / 2))
+  whole__ = whole_ / 2
+  difference = partial_ - whole__
+  if abs(difference) <= imprecision:
+    print("End space: " + str(x))
+    #print(str(partial_) + " " + str(whole_))
+    print("Imprecision: " + str(abs(difference)))
+    print("Pole space: " + str(partial_) + "-" + str(whole__))
 
