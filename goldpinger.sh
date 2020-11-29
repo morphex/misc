9959e8c4160c03f1e3edf797dc496518f53461a5
Morten W. Petersen <morphex@gmail.com>
2020-11-29 10:39:16 +0100

    Adding timeout option, and reboot after n consecutive ping fails

--- goldpinger.sh
+++ goldpinger.sh
@@ -7,6 +7,23 @@
 # first time. For example run after a delay.
 #
 # Has to be run as root.
+#
+# ./goldpinger.sh [timeout] [reboot command]
+#
+# timeout follows the format of sleep
+#
+# Will run reboot command after 30 consecutive failed ping commands
+
+if [ ! -z "$1" ]; then
+  /bin/sleep $1
+fi
+count="0"
+if [ -z "$2" ]; then
+  reboot="/sbin/reboot"
+else
+  reboot=$2
+fi
+
 
 # Getting the active interface
 interface=`route|grep default|awk '{print $8}'`
@@ -15,8 +32,15 @@ do
   ping -c 3 8.8.8.8 > /dev/null 2>&1
   if [ $? -eq 0 ];
   then
-    :
+    count=0
   else
+      count=`expr $count + 1`
+      # Off by one?
+      if [ $count -gt 30 ]; then
+        echo "Rebooting.."
+        (/bin/sleep 5s && $reboot) &
+        exit 1
+      fi
     echo `date -Iseconds` "Ping failed, regetting IP with dhclient"
     /sbin/dhclient $interface
   fi
