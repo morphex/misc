b194c4294ac61f50fa4c03439c7e4d316e7c7546
Morten W. Petersen <morphex@gmail.com>
2021-01-06 23:21:58 +0100

    Adding cron feedback, fixing command syntax

--- check_reboot.py
+++ check_reboot.py
@@ -8,7 +8,7 @@ try:
     logfile = sys.argv[1]
     trigger = sys.argv[2]
 except IndexError:
-    print(str(sys.argv[0]) + ' <logfile> <trigger> <minutes since boot>')
+    print(str(sys.argv[0]) + ' <logfile> <trigger> [minutes since boot]')
     sys.exit(1)
 try:
     MINIMUM_UPTIME = int(sys.argv[3])
@@ -22,4 +22,4 @@ if data.find(trigger) > -1:
   uptime = float(open('/proc/uptime').read().split()[0])
   if uptime > MINIMUM_UPTIME:
     print('Reboot!')
-    os.system('/sbin/reboot')
+    os.system('(/bin/sleep 5s && /sbin/reboot) &')
