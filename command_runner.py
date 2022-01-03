675d9b42ce47703a3389c93aa24c40ce26951af1
Morten W. Petersen <morphex@gmail.com>
2022-01-03 18:55:22 +0100

    Adding script to run command a given number of times

---
+++ command_runner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Small script that will run command N number of times,
+# sleeping seconds between each run.
+
+import sys, os, time
+
+if len(sys.argv) < 4:
+    print("command_runner <N times> <sleep> <command> [command argument 1, 2, ...]")
+    sys.exit(1)
+times = int(sys.argv[1])
+sleep = float(sys.argv[2])
+command = ' '.join(sys.argv[3:])
+for x in range(0, times):
+    os.system(command)
+    time.sleep(sleep)
+
