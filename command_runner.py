6be104eede6553a367f40d167ffe432f1c1bce57
Morten W. Petersen <morphex@gmail.com>
2022-01-13 20:41:10 +0100

    Adding option to repeat forever, some more documentation

--- command_runner.py
+++ command_runner.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 
 # Small script that will run command N number of times,
-# sleeping seconds between each run.  S can be a float,
+# sleeping S seconds between each run.  S can be a float,
 # for example 0.1 (1 10th of a second).
 #
-# For example:
+# If N times is zero, repeat "forever".
+#
+# For example, repeat 10 times 2 seconds apart:
 #
 # ./command_runner.py 10 2 ls
+#
+# Loops forever, 2 seconds apart:
+#
+# ./command_runner.py 0 2 ls
 
-import sys, os, time
+import sys, os, time, itertools
 
 if len(sys.argv) < 4:
     print("command_runner <N times> <S sleep> <command> [command argument 1, 2, ...]")
     sys.exit(1)
 times = int(sys.argv[1])
+if times == 0:
+    repeater = itertools.repeat(1)
+else:
+    repeater = itertools.repeat(1, times=times)
 sleep = float(sys.argv[2])
 command = ' '.join(sys.argv[3:])
-for x in range(0, times):
+for x in repeater:
     os.system(command)
     time.sleep(sleep)
